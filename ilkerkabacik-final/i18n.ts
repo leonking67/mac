e61dabc34494ef2b5078d19bfa8d6058a9c32cf9
leonking67 <koraydark@icloud.com>
2025-10-27 05:30:20 +0300

    Update i18n.ts

--- ilkerkabacik-final/i18n.ts
+++ ilkerkabacik-final/i18n.ts
@@ -1,21 +1,23 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
+import HttpApi from 'i18next-http-backend';
 
 i18n
-  .use(Backend)
+  .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
-    supportedLngs: ['en', 'tr'],
-    interpolation: {
-      escapeValue: false,
-    },
+    supportedLngs: ['tr', 'en', 'ar', 'zh', 'ja'],
+    fallbackLng: 'tr',
+    debug: false,
     backend: {
-      loadPath: '/locales/{{lng}}.json',
+      loadPath: '/locales/{{lng}}.json'
     },
+    detection: {
+      order: ['cookie', 'localStorage', 'htmlTag', 'path', 'subdomain'],
+      caches: ['cookie']
+    }
   });
 
 export default i18n;
